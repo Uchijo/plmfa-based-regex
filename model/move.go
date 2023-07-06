51e39a7201fd99643ca05fc3b2c9e07d23fdfd31
Uchijo <kgtnttki.yuta@gmail.com>
2023-07-07 00:44:37 +0900

    capture作成

--- model/move.go
+++ model/move.go
@@ -1,8 +1,25 @@
 package model
 
+// 各状態から行える操作を示す
 type Move struct {
 	IsEpsilon bool
 	Input     string
 	MoveTo    string
+	CInst     CaptureInstr
 }
 
+// Capturing Groupへの操作を示す
+type CaptureInstr struct {
+	Inst     MemInst
+	MemIndex int
+}
+
+// メモリ操作種別を示すenum
+type MemInst int
+
+const (
+	// Keep is default
+	Keep MemInst = iota
+	Close
+	Open
+)
---
+++ model/reg_capture.go
@@ -0,0 +1,54 @@
+package model
+
+import "github.com/google/uuid"
+
+type RegCapture struct {
+	RegExp
+	// captureする正規表現
+	Content RegExp
+	// メモリのindex
+	MemoryIndex int
+}
+
+func (rc RegCapture) States(startId string) ([]State, string, error) {
+	// fixme: エラー処理直す
+	contentSId, _ := uuid.NewRandom()
+	entryState := State{
+		Id: startId,
+		Moves: []Move{
+			{
+				IsEpsilon: true,
+				Input:     "",
+				MoveTo:    contentSId.String(),
+				CInst: CaptureInstr{
+					Inst:     Open,
+					MemIndex: rc.MemoryIndex,
+				},
+			},
+		},
+		IsEnd: false,
+	}
+
+	cs, endId, _ := rc.Content.States(contentSId.String())
+
+	exitId, _ := uuid.NewRandom()
+	exitState := State{
+		Id:    endId,
+		IsEnd: false,
+		Moves: []Move{
+			{
+				IsEpsilon: true,
+				Input:     "",
+				MoveTo:    exitId.String(),
+				CInst: CaptureInstr{
+					Inst:     Close,
+					MemIndex: rc.MemoryIndex,
+				},
+			},
+		},
+	}
+
+	cs = append(cs, entryState, exitState)
+
+	return cs, exitId.String(), nil
+}
