0b021948612d798e1f82fcaff10cdf7c55acf95e
Uchijo <kgtnttki.yuta@gmail.com>
2023-11-01 00:37:07 +0900

    文字集合の扱いの指針がたった

--- model/reg_charset.go
+++ model/reg_charset.go
@@ -1,40 +1,55 @@
 package model
 
+// 文字集合を表す構造体
 type RegCharSet struct {
-	// 先頭に ^ がついていない -> true
-	WhiteList bool
-
 	// 対象文字
-	Content IsInner
+	Content CharContainer
 }
 
 func (rc RegCharSet) States(startId string) ([]State, string, error) {
 	return []State{}, "", nil
 }
 
-type IsInner interface {
+type CharContainer interface {
 	// IsIn tests if [c] is in the specified character set or not.
-	IsIn(c rune) bool
+	Contains(c rune) bool
 }
 
+// [a-z]タイプの文字集合
 type CharRange struct {
 	Start rune
 	End   rune
+
+	// ^が先頭にない -> true
+	WhiteList bool
 }
 
-func (cr CharRange) IsIn(c rune) bool {
+func (cr CharRange) Contains(c rune) bool {
 	return cr.Start <= c && c <= cr.End
 }
 
+// [abc]タイプの文字集合
 type CharList struct {
 	Chars []rune
+
+	// ^が先頭にない -> true
+	WhiteList bool
 }
 
-func (cl CharList) IsIn(c rune) bool {
+func (cl CharList) Contains(c rune) bool {
 	for _, val := range cl.Chars {
 		if val == c {
 			return true
 		}
 	}
 	return false
+}
+
+// 1文字だけ、[]なしでも使えるやつ
+type SingleChar struct {
+	Char rune
+}
+
+func (sc SingleChar) Contains(c rune) bool {
+	return sc.Char == c
 }
