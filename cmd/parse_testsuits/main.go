90c6076d6091d6b99a547e1573c4c6f023599fa6
Uchijo <kgtnttki.yuta@gmail.com>
2023-11-02 03:34:38 +0900

    wip: テスト抽出ツール作成中

---
+++ cmd/parse_testsuits/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/antlr4-go/antlr/v4"
+	"github.com/uchijo/plmfa-based-regex/model"
+	"github.com/uchijo/plmfa-based-regex/parser"
+	gen "github.com/uchijo/plmfa-based-regex/parser/gen"
+)
+
+func main() {
+	hoge := canParsePattern("aaa")
+	fmt.Printf("hoge: %v\n", hoge)
+
+	b, err := os.ReadFile("tests/test1")
+	if err != nil {
+		panic("error" + err.Error())
+	}
+	rawContents := string(b)
+	unfilteredContents := strings.Split(rawContents, "\n\n")
+	filteredTests := []string{}
+	for _, v := range unfilteredContents {
+		if !isTestCase(v) {
+			continue
+		}
+		filteredTests = append(filteredTests, v)
+	}
+
+	fmt.Println("hogehoge!!!!")
+	tests := []Test{}
+	for _, v := range filteredTests {
+		tests = append(tests, extractTest(v))
+	}
+
+	fmt.Printf("tests length: %v\n", len(tests))
+
+	for _, v := range tests {
+		if !v.CanHandle {
+			continue
+		}
+		fmt.Println(v.StrippedPattern)
+	}
+}
+
+func isTestCase(input string) bool {
+	// 最初が/でない場合違う
+	return input[:1] == "/"
+}
+
+func extractTest(input string) Test {
+	lines := strings.Split(input, "\n")
+	rawPattern := lines[0]
+	canHandle := canHandlePattern(rawPattern)
+	stripped := ""
+	if canHandle {
+		stripped = extractPattern(rawPattern)
+	}
+	return Test{
+		RawPattern:      rawPattern,
+		CanHandle:       canHandle,
+		StrippedPattern: stripped,
+	}
+}
+
+var caret = regexp.QuoteMeta("^")
+var dollar = regexp.QuoteMeta("$")
+var pattern = "/" + caret + "(.*)" + dollar + "/"
+var compiledRe, _ = regexp.Compile(pattern)
+
+func extractPattern(input string) string {
+	matches := compiledRe.FindAllStringSubmatch(input, -1)
+	for _, v := range matches {
+		return v[1]
+	}
+	panic("something went wrong")
+}
+
+func canHandlePattern(input string) bool {
+	// ^$で囲まれているか確認
+	// match, err := regexp.Match("/^.*$/[dgimsuy]{0,6}", []byte(input))
+	match, err := regexp.Match("^/"+caret+".*"+dollar+"/g?$", []byte(input))
+	if err != nil {
+		panic("match failed")
+	}
+	return match
+}
+
+// 途中でパニックが発生 -> パース不可 -> recoverでfalseが帰る
+// 参考: https://h3poteto.hatenablog.com/entry/2015/12/13/010000
+func canParsePattern(input string) bool {
+	if input == "" {
+		return false
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			// do nothing
+		}
+	}()
+
+	is := antlr.NewInputStream(input)
+	lexer := gen.NewPCRELexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	p := gen.NewPCREParser(stream)
+	p.BuildParseTrees = true
+	tree := p.Pcre()
+	_ = tree.Accept(&parser.RegexBuilder{}).(model.RegExp)
+
+	return true
+}
+
+type Test struct {
+	RawPattern       string
+	StrippedPattern  string
+	PositiveExamples []string
+	NegativeExamples []string
+	CanHandle        bool
+}
